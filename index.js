3e68b0d3c99e54fcf604bc210a3364fc530f0708
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-07-31 11:17:07 +0200

    Store message-el in variable and remove emojies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // 1. Create two variables, firstCard and secondCard. 
 // Set their values to a random number between 2-11
-let firstCard = 11
+let firstCard = 12
 let secondCard = 9
 // 2. Create a variable, sum, and set it to the sum of the two cards
 let sum = firstCard + secondCard
@@ -8,12 +8,17 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if (sum < 21) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+let messageEl = document.getElementById("message-el")
+
+function startGame() {
+    if (sum < 21) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
