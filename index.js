f1c27cbc433868dc35f5479e25a591bf78cc6b7b
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-07-31 19:10:41 +0200

    Reassign varuiables to startGame function

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
-let firstCard = 12
-let secondCard = 9
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+function getRandomCard() {
+    let randomNumber =  Math.floor( Math.random()*13 ) + 1 // generate number from 1-13
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
@@ -38,7 +52,7 @@ function renderGame() {
 
 function newCard() {
     console.log("Drawing a new card from the deck!")
-    let card = 6
+    let card = getRandomCard()
     sum += card
     cards.push(card)
     renderGame()
