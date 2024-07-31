f43bb58d4df27a8e035e98ffe5c8ef5a53e985e1
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-07-31 10:29:13 +0200

    Add new variables and link to external CSS file

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 // 1. Create two variables, firstCard and secondCard. 
 // Set their values to a random number between 2-11
-let firstCard = 6
+let firstCard = 11
 let secondCard = 9
 // 2. Create a variable, sum, and set it to the sum of the two cards
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
 if (sum < 21) {
-    console.log("Do you want to draw a new card? 🙂")
+    message = "Do you want to draw a new card? 🙂"
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
 } else {
-    console.log("You're out of the game! 😭")
+    message = "You're out of the game! 😭"
+    isAlive = false
 }
