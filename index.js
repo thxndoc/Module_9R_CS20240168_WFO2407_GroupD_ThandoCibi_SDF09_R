24fadae61126233b994fde6efdbf1d8cd85b9eb3
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-07-31 15:13:48 +0200

    Create array for cards

--- index.js
+++ index.js
@@ -2,15 +2,23 @@
 // Set their values to a random number between 2-11
 let firstCard = 12
 let secondCard = 9
-// 2. Create a variable, sum, and set it to the sum of the two cards
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    sumEl.textContent = "Sum: " + sum
+    //conditionals
     if (sum < 21) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -21,4 +29,11 @@ function startGame() {
         isAlive = false
     }
     messageEl.textContent = message
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+    let card = 6
+    sum += card
+    renderGame()
 }
